9f9e62db9f77bc66971ae22906ebafd3cfadded0
yactouat <yacine.touati.pro@gmail.com>
2024-05-31 16:28:28 +0200

    session2

---
+++ session2/program2.py
@@ -0,0 +1,7 @@
+# COMMENTAIRE: cette variable sert à stocker le nom du récipendiaire du mail
+prenom1 = "Laura"
+prenom2 = "PM"
+
+ma_variable = f"{prenom1} est allée se promener avec {prenom2}"
+
+print(ma_variable)
